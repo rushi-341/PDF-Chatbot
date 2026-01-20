62f55202fec0de193be695fa230b46518030294e
Yalamanchili Rushi <122519375+rushi-341@users.noreply.github.com>
2026-01-20 10:21:01 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -8,7 +8,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from langchain_groq import ChatGroq
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import load_qa_chain
 from langchain.prompts import PromptTemplate
 
 # --------------------------------------------------
